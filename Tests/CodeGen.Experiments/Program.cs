7742e7da192c380d45b4e4743f7d6b216646f24e
Marvin Drude <marvin@gidd.io>
2025-08-28 19:16:36 +0200

    Add missing files

---
+++ Tests/CodeGen.Experiments/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using CodeGen.Writing;
+
+Console.WriteLine("Hello, World!");
+
+var writer = new CodeTextWriter(
+   stackalloc char[256], stackalloc char[128]);
+  
+var x = 20;
+
+writer.UpIndent();
+writer.UpIndent();
+writer.OpenBody();
+writer.WriteInterpolated($"{x} - {x}");
+writer.WriteLine();
+writer.CloseBodySemicolon();
+
+Console.WriteLine(writer.ToString());
